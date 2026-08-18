c630b83611ff819a0f0f40b84bc50c3feef6076d
Marc Breedt <marc@optimumgroup.co.za>
2026-08-18 15:02:36 +0200

    Resolve merge conflict by incorporating both suggestions

--- src/Meridian.Api/Program.cs
+++ src/Meridian.Api/Program.cs
@@ -4,11 +4,38 @@ using Microsoft.Identity.Web;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+// CORS for Blazor WASM client
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MeridianClient", policy =>
+    {
+        policy
+            .WithOrigins(
+                "https://localhost:7274",
+                "http://localhost:5274")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+// Microsoft Entra authentication
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(
+        builder.Configuration.GetSection("AzureAd"));
+
 builder.Services.AddAuthorization();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LocalDev", policy =>
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
 
 var app = builder.Build();
 
@@ -18,8 +45,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("LocalDev");
+
+app.UseCors("MeridianClient");
+
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
